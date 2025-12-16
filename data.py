3e3be914f37d5aff13cddc7c4305e3c2f688d3df
GujjiLadakh <nachi@gujji.net>
2025-12-16 20:35:38 +0000

    Add gravitational constants and time variables

---
+++ data.py
@@ -0,0 +1,16 @@
+G: float           = 6.67e-11              # Constant of Gravitation
+
+AU: float          = 1.5e11                # Astronomical Unit in metres
+
+M_s: float         = 2.0e30                # Mass of Sun
+M_e: float         = 5.972e24              # Mass of Earth
+# M_m: float         = 3.301e23              # Mass of Mercury
+# M_v: float         = 4.867e24              # Mass of Venus
+
+v_e_aph: int       = 29290                 # Velocity of Earth at aphelion
+# v_m_aph: int       = 38860                 # Velocity of Mercury at aphelion
+# v_v_aph: int       = 34790                 # Velocity of Venus at aphelion
+
+day_seconds: float = 24.0 * 60 * 60        # Number of seconds in a day
+t: float           = 0.0                   # Counter
+dt: float          = 1 * day_seconds       # Time step
